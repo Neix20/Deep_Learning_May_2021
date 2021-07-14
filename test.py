4d39766256c713592d112ccc65e917f4dc19f270
lee-victor23 <87189978+lee-victor23@users.noreply.github.com>
2021-07-14 18:10:31 +0800

    testing

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello")
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from torchsummary import summary
+
+from cifar10 import CIFAR10
