047438cbaa21dcac3709f3663d41867c296e0491
lee-victor23 <87189978+lee-victor23@users.noreply.github.com>
2021-07-14 22:25:21 +0800

    test

--- test.py
+++ test.py
@@ -6,23 +6,43 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torchvision
+from torchvision.datasets.mnist import FashionMNIST
 import torchvision.transforms as transforms
 
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from torchsummary import summary
- 
-trainset =  torchvision.datasets.FashionMNIST(root=".",transform=None, train= True,download= True)
-testset  = torchvision.datasets.FashionMNIST(root=".", transform=None,train=False, download=True)
 
+transform = transforms.Compose([
+transforms.ToTensor(),
+transforms.Normalize((0.5, 0.5, 0.5), (1.0, 1.0, 1.0))
+])
+trainset = FashionMNIST(root=".",transform=transform, train= True,download= True)
+testset  = FashionMNIST(root=".",transform=transform,train= False, download=True)
 
 img, label = trainset.data[3], trainset.targets[3]
 
-print(img.dtype)
-print(img.shape)
-
+print(trainset.classes)
 plt.title(trainset.classes[label])
-plt.imshow(img)
+plt.imshow(img, cmap=plt.get_cmap('gray'))
 _ = plt.axis("off")
 plt.show()
+'''
+for i in range(9):
+	# define subplot
+	plt.subplot(330 + 1 + i)
+	# plot raw pixel data
+	plt.imshow(trainset.data[i], cmap=plt.get_cmap('gray'))
+# show the figure
+plt.show()
+'''
+
+print(trainset.classes)
+
+def display(one_digit):
+    one_digit_image = one_digit.reshape(28,28)
+    plt.imshow (one_digit_image, cmap = matplotlib.cm.gray, interpolation = 'nearest')
+    
+one_digit = trainset.data[0]  # change number (0~69999) to show other images
+display(one_digit)
